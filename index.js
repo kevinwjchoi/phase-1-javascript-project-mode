16e049e54a01b3280b21f4f9c464d8e558aca6b4
Kevin <kevinnwjchoi@gmail.com>
2024-03-26 17:09:48 -0400

    commenting the different sections of my project

--- index.js
+++ index.js
@@ -2,3 +2,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Hello");
 })
+
+//GET Fetch
+
+
+//Render Dog card
+
+
+//POST Fetch
+
+
+//DELETE Fetch
+
+
+//Callback Functions 
+
+
+//Submit EventListener
+
+
+//Click EventListener
+
+
+//Mouseover EventListener 
+
