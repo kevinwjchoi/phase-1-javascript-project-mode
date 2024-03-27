305547a7a806e8aed60ac01e56bde27de526251f
Kevin <kevinnwjchoi@gmail.com>
2024-03-27 17:39:32 -0400

    added to the DOM

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ function renderDogs(dog){
     dogContainer.id = (`container${dog.name}`);
     dogCard.appendChild(dogContainer);
 
+    //creating html elements for each dog 
+    const h2 = document.createElement("h2");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+
+    //appended to DOM 
+    dogContainer.appendChild(h2);
+    dogContainer.appendChild(p1);
+    dogContainer.appendChild(p2);
+    dogContainer.appendChild(p3);
+    h2.textContent = dog.name;
+    p1.textContent = dog.description;
+    p2.textContent = dog.location;
+    p3.textContent = dog.image;
 
 }
 
