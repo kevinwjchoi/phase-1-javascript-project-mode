87888f186c9b22a0ba6943a62c1bcc8cb0d709c6
Kevin <kevinnwjchoi@gmail.com>
2024-03-27 16:40:13 -0400

    added html elements

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-//DOMContentLoaded 
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello");
-})
+ //GET Fetch
 
-//GET Fetch
-
-
-//Render Dog card
-
-
-//POST Fetch
 
 
 //DELETE Fetch
 
-
 //Callback Functions 
-
-
-//Submit EventListener
-
-
-//Click EventListener
-
 
 //Mouseover EventListener 
 
